35466212f2146dc04fbc3bd91ee3d251daab9880
Leitetc <thiagoleite22@hotmail.com>
2023-11-11 23:56:46 -0300

    Questao5 finalizada

--- Semana1/Questao5/Program.cs
+++ Semana1/Questao5/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!5");
+Console.WriteLine("Questão 05. Operadores de Comparação:");
+int a = 5;
+int b = 8;
+
+if (a > b)
+{
+    Console.WriteLine("a é maior que b");
+}
+else
+{
+    Console.WriteLine("b é maior que a");
+}
