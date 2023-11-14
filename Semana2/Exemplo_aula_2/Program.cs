431baf6c9c4cb635cc6aee2cefde85534ccc52e3
Leitetc <thiagoleite22@hotmail.com>
2023-11-14 15:15:35 -0300

    commit

---
+++ Semana2/Exemplo_aula_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
+#region Divisiveis
+
+ Console.WriteLine("Números divisíveis por 3 ou 4 entre 0 e 30:");
+
+        for (int Resto = 0; Resto <= 30; Resto++)
+        {
+            if (Resto % 3 == 0 || Resto % 4 == 0)
+            {
+                Console.WriteLine(Resto);
+            }
+        }
+
+#endregion
+
+#region Fibonacci
+#endregion
