ce7f2fb2a5a6d50cf04fa0cefb6f724fab36bb42
Leitetc <thiagoleite22@hotmail.com>
2023-11-14 15:25:38 -0300

    commit

--- Semana2/Exemplo_aula_2/Program.cs
+++ Semana2/Exemplo_aula_2/Program.cs
@@ -15,5 +15,33 @@ using System.Security.Cryptography;
 
 #endregion
 
-#region Fibonacci
+﻿
+/* #region Foreach examples
+
+string[] people = { "Maria", "João", "José", "Ana" };
+
+foreach(string person in people)
+{
+    Console.WriteLine(person);
+}
 #endregion
+ */
+
+#region Fibonacci
+int fib1 = 0;
+int fib2 = 1;
+int fib3;
+
+Console.WriteLine(fib1);
+Console.WriteLine(fib2);
+
+fib3 = fib2+fib1;
+
+while (fib3 <= 100)
+{
+    Console.WriteLine(fib3);
+    fib1 = fib2;
+    fib2 = fib3;
+    fib3 = fib2 + fib1;
+}
+ #endregion
